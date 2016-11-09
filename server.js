2b5c11d1257fc7fe15a55595ee144863f49bbb45
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-11-09 20:06:33 +0100

    First version

---
+++ server.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Albert_FX on 09/11/2016.
+ */
+
+/*
+Vamos a utilizar un modulo, llamado express, que nos permitirá de una forma sencilla hacer un servicio web
+ */
+var express = require('express');
+
+/*
+    Con la instancia var app creamos nuestro servidor.
+ */
+var app = express();
+
+/*
+    Definimos una función de callback que se va a ejecutar cada vez que haga alguien un get al servidor
+    req: Petición
+    res: Objeto de la respuesta
+ */
+app.get('/', function(req, res){
+    res.send("Hello world from server");
+});
+
+app.listen(1000);
+
+console.log("Running...");
