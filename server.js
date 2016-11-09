823a091f37e8be1c222c27cba7ba31ad305ff93c
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-11-09 20:16:17 +0100

    Api REST contact example

--- server.js
+++ server.js
@@ -2,34 +2,32 @@
  * Created by Albert_FX on 09/11/2016.
  */
 
-/*
-Vamos a utilizar un modulo, llamado express, que nos permitirá de una forma sencilla hacer un servicio web
- */
+//Vamos a utilizar un modulo, llamado express, que nos permitirá de una forma sencilla hacer un servicio web
+
 var express = require('express');
 
-/*
-    Con la instancia var app creamos nuestro servidor.
- */
+//Modifica un objeto javascript a JSON
+var bodyParser = require('body-parser');
+
+
+//Con la instancia var app creamos nuestro servidor.
 var app = express();
+var apiBaseURL = "/api/v1";
 
+var contact1 = { name: "alberto", phone: 123456789 };
+var contact2 = { name: "luis", phone: 321654987 };
 
-var contact1 = {
-    name: "alberto",
-    phone: 123456789
-};
-var contact2 = {
-    name: "luis",
-    phone: 321654987
-};
+var contacts = [contact1, contact2];
 
+app.use(bodyParser.json());
 
 /*
     Definimos una función de callback que se va a ejecutar cada vez que haga alguien un get al servidor
     req: Petición
     res: Objeto de la respuesta
  */
-app.get('/', function(req, res){
-    res.send("Hello world from server");
+app.get(apiBaseURL+'/contacts', function(req, res){
+    res.json(contacts);
 });
 
 app.listen(1000);
