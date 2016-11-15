490b838df4ea6cd5ef88139ff3978778c23e9b14
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-11-15 20:33:45 +0100

    Finalizada sesión creando fichero en disco para persistencia

--- server.js
+++ server.js
@@ -2,24 +2,32 @@ var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 var dataStore  = require("nedb");
+var path       = require("path");
 
 var port = process.env.PORT || 10000;
-
-//Con la instancia var app creamos nuestro servidor.
 var baseApi = "/api/v1";
+var dbFileName = path.join(__dirname,'contacts.json');
 
 //Esto nos crea un objeto donde introducir información.
 // Si no indico nada to-do esto lo hace en memoria pero luego veremos como añadirlo en un fichero
-var db = new dataStore();
+var db = new dataStore({
+    filename: dbFileName,
+    autoload: true
+});
 
 cl("DB initialized");
 
-var contacts = [{ name: "pedro", phone: 123456789, email: "pedro@pedro.com"},
-                { name: "pepe",  phone: 611112223, email: "pepe@pepe.com"  }
-               ];
+db.find({}, (err, contacts)=>{
+    if(contacts.length == 0){
+        db.insert([{ name: "pedro", phone: 123456789, email: "pedro@pedro.com"},
+                   { name: "pepe",  phone: 611112223, email: "pepe@pepe.com"  }
+                  ]);
+    }else{
+        cl("Loaded DB with "+contacts.length+" contacts");
+    }
+});
 
-db.insert(contacts);
-cl("Inserted 2 default contacts");
+
 app.use(bodyParser.json());
 
 app.get(baseApi+'/contacts', (req, res)=>{
