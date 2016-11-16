a5eb9fd734a9caab0b620dec69b2c0b412255523
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-11-16 18:28:54 +0100

    Añadido ngRepeat con contacts de ejemplo

--- public/list-ctrl.js
+++ public/list-ctrl.js
@@ -3,4 +3,6 @@ angular.module("ContactListApp")
         console.log("List controller initialized");
 
         $scope.test = "test";
+        $scope.contactList = [{ name: "pedro", phone: 123456789, email: "pedro@pedro.com"},
+                              { name: "pepe",  phone: 611112223, email: "pepe@pepe.com"  }];
     });
