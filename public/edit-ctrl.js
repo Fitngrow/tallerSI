90ef816942fe83c9ff28de93b105a5e2b156a968
AlbertFX91 <alberto.rojas.fndez@gmail.com>
2016-11-16 20:19:07 +0100

    Adding cool notification and redirect

--- public/edit-ctrl.js
+++ public/edit-ctrl.js
@@ -1,15 +1,27 @@
 angular.module("ContactListApp")
-    .controller("EditCtrl", function($scope, $http, $routeParams){
+    .controller("EditCtrl", function($scope, $http, $routeParams, $location){
         console.log("Edit controller initialized");
 
         $http.get("/api/v1/contacts/"+$routeParams.name).success(function(contact){
             $scope.contact = contact;
-        })
+        });
 
         $scope.updateContact = function(){
             var contact = $scope.contact;
             $http.put("/api/v1/contacts/"+contact.name, contact).success(function(e){
-                console.log("TODO OK");
+                showSnackbar("Contact edited");
+                $location.path("/")
             })
+        };
+
+        function showSnackbar(message){
+                var message = message || "Hello world";
+                // Get the snackbar DIV
+                var x = document.getElementById("snackbar");
+                // Add the "show" class to DIV
+                x.className = "show";
+                x.innerHTML = message;
+                // After 3 seconds, remove the show class from DIV
+                setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
         }
     });
